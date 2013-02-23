ff078d2a27033d077e245314a3dfe74091e116a6
Douglas Huck <faceguydb@gmail.com>
2013-02-23 06:17:59 -0500

    Added Files

---
+++ lua/autorun/server/dougsCrash.lua
@@ -0,0 +1,3 @@
+AddCSLuaFile( "autorun/client/dougsCrash.lua" )
+AddCSLuaFile( "autorun/shared/dougsCrash.lua" )
+include( "autorun/shared/dougsCrash.lua" )
---
+++ lua/autorun/shared/dougsCrash.lua
@@ -0,0 +1,18 @@
+local maxDist = 10
+local _R = debug.getregistry()
+local oldentsetpos = _R.Entity.SetPos
+function _R.Entity:SetPos(pos)
+	fixPositions(pos)
+	oldentsetpos(self, pos)
+end
+local oldphysobjsetpos = _R.PhysObj.SetPos
+function _R.PhysObj:SetPos(pos)
+	fixPositions(pos)
+	oldphysobjsetpos(self, pos)
+end
+function fixPositions(pos)
+	if pos.x > maxDist then pos.x = maxDist end
+	if pos.y > maxDist then pos.y = maxDist end
+	if pos.z > maxDist then pos.z = maxDist end
+	return pos
+end
